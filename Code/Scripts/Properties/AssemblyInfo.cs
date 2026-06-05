82638f0352aa92e7df723ca104d9bd8ce66a87a0
Janine Dirichlet <19920458+virtuNat@users.noreply.github.com>
2026-06-05 15:05:03 +0800

    Update metadata to reflect current status

--- Code/Scripts/Properties/AssemblyInfo.cs
+++ Code/Scripts/Properties/AssemblyInfo.cs
@@ -2,14 +2,14 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyTitle("virtu9WCorpPlus")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyTitle("virtu9_WCorpPlus")]
 [assembly: AssemblyDescription("Yet another take on a WARP Corp reception.")]
 [assembly: AssemblyConfiguration("release")]
 [assembly: AssemblyCompany("virtuNat")]
-[assembly: AssemblyProduct("virtu9WCorpPlus")]
+[assembly: AssemblyProduct("virtu9_WCorpPlus")]
 [assembly: AssemblyCopyright("Copyright © virtuNat 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("45c7655b-6fd7-47e0-9324-6802a47ca84a")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
